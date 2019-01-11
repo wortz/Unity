5bfb9b34707ef128e54b6ce3c82800376ec3d174
wortz <jp_pp_hi5@hotmail.com>
2019-01-11 06:54:34 +0000

    first commit

---
+++ Tutorial/2D clicker/Assets/Scripts/ClickButton.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButton : MonoBehaviour
+{
+    public void ButtonClicked(){
+        GlobalCookies.CookieCount += 1;
+    }
+}
